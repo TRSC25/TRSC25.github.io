2190b271d9a56d190156a5e7897b8f5f0dd28370
TRSC25 <ishan.myapp@gmail.com>
2023-09-08 16:42:48 +0530

    Fix SW1

--- service-worker.js
+++ service-worker.js
@@ -4,10 +4,10 @@ self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open(cacheName).then(function(cache) {
       return cache.addAll([
-        '/js/bootstrap.min.js',
-        '/js/s.w.js',
-        '/js/service-worker.js',
-        '/css/bootstrap.min.css',
+        './js/bootstrap.min.js',
+        './js/s.w.js',
+        './js/service-worker.js',
+        './css/bootstrap.min.css',
         // Add other assets you want to cache
       ]);
     })
@@ -18,9 +18,9 @@ self.addEventListener('activate', function(event) {
   event.waitUntil(
     caches.keys().then(function(cacheNames) {
       return Promise.all(
-        cacheNames.map(function(cacheName) {
-          if (cacheName !== cacheVersion) {
-            return caches.delete(cacheName);
+        cacheNames.map(function(existingCacheName) {
+          if (existingCacheName !== cacheName) {
+            return caches.delete(existingCacheName);
           }
         })
       );
