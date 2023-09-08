aabd367f68556f019762b087044cca549ccffec3
TRSC25 <ishan.myapp@gmail.com>
2023-09-08 19:56:34 +0530

    rebuilt service worker

--- service-worker.js
+++ service-worker.js
@@ -1,15 +1,46 @@
-const addResourcesToCache = async (resources) => {
-  const cache = await caches.open("v1");
-  await cache.addAll(resources);
-};
+const cacheName = "v1";
 
-self.addEventListener("install", (event) => {
-  event.waitUntil(
-    addResourcesToCache([
-      './js/bootstrap.min.js',
-      './js/s.w.js',
-      './js/service-worker.js',
-      './css/bootstrap.min.css',
-    ]),
+const appShellFiles = [
+  "./service-worker.js",
+  "./js/s.w.js"
+];
+
+self.addEventListener("install", (e) => {
+  console.log("[Service Worker] Install");
+  e.waitUntil(
+    (async () => {
+      const cache = await caches.open(cacheName);
+      console.log("[Service Worker] Caching all: app shell and content");
+      await cache.addAll(appShellFiles);
+    })(),
+  );
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    (async () => {
+      const r = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (r) {
+        return r;
+      }else{
+        console.log(`[Service Worker] Resource at ${e.request.url} isn't set for caching.`);
+      }
+    })(),
+  );
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList.map((key) => {
+          if (key === cacheName) {
+            return;
+          }
+          return caches.delete(key);
+        }),
+      );
+    }),
   );
 });
