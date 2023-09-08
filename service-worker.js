6e3814650c11c249157274d9cc9f59e1e8600123
TRSC25 <ishan.myapp@gmail.com>
2023-09-08 16:49:14 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,37 +1,15 @@
-const cacheName = 'v1';
+const addResourcesToCache = async (resources) => {
+  const cache = await caches.open("v1");
+  await cache.addAll(resources);
+};
 
-self.addEventListener('install', function(event) {
+self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(cacheName).then(function(cache) {
-      return cache.addAll([
-        './js/bootstrap.min.js',
-        './js/s.w.js',
-        './js/service-worker.js',
-        './css/bootstrap.min.css',
-        // Add other assets you want to cache
-      ]);
-    })
-  );
-});
-
-self.addEventListener('activate', function(event) {
-  event.waitUntil(
-    caches.keys().then(function(cacheNames) {
-      return Promise.all(
-        cacheNames.map(function(existingCacheName) {
-          if (existingCacheName !== cacheName) {
-            return caches.delete(existingCacheName);
-          }
-        })
-      );
-    })
-  );
-});
-
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request).then(function(response) {
-      return response || fetch(event.request);
-    })
+    addResourcesToCache([
+      './js/bootstrap.min.js',
+      './js/s.w.js',
+      './js/service-worker.js',
+      './css/bootstrap.min.css',
+    ]),
   );
 });
