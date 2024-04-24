9b6e0c634c39809ac0fc0d73e65d97167e55add9
TRSC25 <ishan.myapp@gmail.com>
2024-04-24 14:42:42 +0530

    Yet an other UI update  :/
    Im retarded.

--- js/s.w.js
+++ js/s.w.js
@@ -1,3 +1,4 @@
-if ("serviceWorker" in navigator) {
-  navigator.serviceWorker.register("./service-worker.js");
-}
+//if ("serviceWorker" in navigator) {
+//  navigator.serviceWorker.register("./service-worker.js");
+//}
+console.log("Service worker has been disabled for reasons idk")
